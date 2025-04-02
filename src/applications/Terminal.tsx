4be8a4242fef69216492733fd6bba81b41c1c3af
bitbyte08 <eight2823@naver.com>
2025-04-02 10:26:18 +0900

    feat :: html Terminal 구현

--- src/applications/Terminal.tsx
+++ src/applications/Terminal.tsx
@@ -14,31 +14,70 @@ const Input = styled.input`
 const TerminalContent = styled.div`
     display: flex;
     align-items: flex-end;
+    flex-direction: column;
     background-color: black;
     height: 100%;
     width: 100%;
+`
+const CommandContent = styled.div`
+    display: flex;
+    height: 100%;
+    flex-direction: column-reverse;
+    width: 100%;
+    
+`
+const LastCommand = styled.p`
+    width: 100%;
+    color: white;
+    display: flex;
+    margin: 0;
+    padding: 0;
 `
 
 const Terminal = () =>{
   const [command,setCommand] = useState("");
   const [history, Push ,Pop,] = useQueue();
-
+  const [session, setSession] = useState<string[]>([]);
+  useEffect(() => {
+    let res:any;
+    if(command == "history"){
+      res = GetHistory();
+      console.log(res);
+    }else{
+      res = command;
+    }
+    setSession([res,...session]);
+    console.log(command);
+  }, [command]);
   useEffect(() => {
     if(history.length > 100){
       Pop();
     }
   },[history]);
 
+  const GetHistory:()=>string[] = () => {
+    let str="";
+    for(let i=0;i<history.length;i++){
+      str += history[i] + "<br/>";
+    }
+    return str;
+  }
+
   return (
     <TerminalContent>
-      <Input onKeyDown={(e)=>{
+      <CommandContent>
+      {session.map((item,index)=>(
+          <LastCommand key={index}>
+            <p dangerouslySetInnerHTML={{__html: item}} />
+          </LastCommand>
+      ))}
+      </CommandContent>
+      <Input onKeyDown={(e:any)=>{
         if(e.key==="Enter") {
-          console.log("enter");
+          setCommand(e.target.value);
           Push(e.target.value);
-          console.log(history);
           e.target.value = "";
         }
-        setCommand(e.target.value);
         }}></Input>
     </TerminalContent>
   )
