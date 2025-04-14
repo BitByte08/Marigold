39974f77be35d1d01f7bfbd1e4c1e630bf077e57
bitbyte08 <eight2823@naver.com>
2025-04-14 17:40:38 +0900

    feat :: 함수 actions로 분리

--- src/modules/taskModule.ts
+++ src/modules/taskModule.ts
@@ -1,25 +1,31 @@
 import {create} from "zustand/react";
 import {TaskType} from '@/modules/typeModule.tsx'
 
-interface TaskListInterface {
+interface TaskListStateInterface {
   TaskList: TaskType[];
-  addTask: (component: TaskType) => void;
-  removeTask: (component: TaskType) => void;
+}
+interface TaskListActionInterface {
+  actions: {
+    addTask: (component: TaskType) => void;
+    removeTask: (component: TaskType) => void;
+  }
 }
 
-const useTaskListStore = create<TaskListInterface>(set=>(
+const useTaskListStore = create<TaskListStateInterface & TaskListActionInterface>(set=>(
   {
     TaskList: [],
-    addTask: (component: TaskType) => {
-      set(state => ({
-        TaskList: ((!state.TaskList.includes(component)) ? [...state.TaskList, component] : [...state.TaskList])
-      }))
-    },
-    removeTask: (component: TaskType) => {
-      set(state => ({
-        TaskList: ((state.TaskList.some(item => item.name === component.name)) ?
-          state.TaskList.filter(item => item.name !== component.name) : [...state.TaskList])
-      }))
+    actions: {
+      addTask: (component: TaskType) => {
+        set(state => ({
+          TaskList: ((!state.TaskList.includes(component)) ? [...state.TaskList, component] : [...state.TaskList])
+        }))
+      },
+      removeTask: (component: TaskType) => {
+        set(state => ({
+          TaskList: ((state.TaskList.some(item => item.name === component.name)) ?
+            state.TaskList.filter(item => item.name !== component.name) : [...state.TaskList])
+        }))
+      }
     }
   }
 ))
